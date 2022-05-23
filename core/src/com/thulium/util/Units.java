f18c793f726a7d0b8de91ef17117d9edee6fdee1
Brandin Hand <brandinfoot@gmail.com>
2022-05-23 18:59:29 -0400

    Added units
    Added units, changed game size

--- core/src/com/thulium/util/Units.java
+++ core/src/com/thulium/util/Units.java
@@ -1,14 +1,16 @@
 package com.thulium.util;
 
 public final class Units {
-	public static final int WIDTH = 16;
-	public static final int HEIGHT = 10;
+	public static final int WIDTH = 8;
+	public static final int HEIGHT = 5;
 	public static final int JUMP = 12;
 	public static final float MAX_VELOCITY = 5;
 	public static final short ALL_FLAG = 1 << 0;
 	public static final short NONE_FLAG = Short.MAX_VALUE;
 	public static final short GROUND_FLAG = 1 << 2;
 	public static final short ENTITY_FLAG = 1 << 3;
+
+	public static final float AMP_MASS = 0;
 
 	public static final String[] animations = { "idle", "run", "jump_up", "jump_down" };
 
