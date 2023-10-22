f588a52d2fbab3d5be4580a8eec1400424fbfb7c
MuadDib9 <106213940+MuadDib9@users.noreply.github.com>
2023-10-22 02:24:06 -0400

    update map
    update map

--- android/assets/maps/spawn_layer.tsx
+++ android/assets/maps/spawn_layer.tsx
@@ -1,9 +1,18 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="spawn_layer" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+<tileset version="1.5" tiledversion="1.6.0" name="spawn_layer" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="spawn_layer.png" width="512" height="512"/>
  <tile id="0">
   <properties>
+   <property name="height" type="float" value="1"/>
    <property name="spawn" value="squirrel"/>
+   <property name="width" type="float" value="1"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="height" type="float" value="1"/>
+   <property name="spawn" value="coon"/>
+   <property name="width" type="float" value="1"/>
   </properties>
  </tile>
 </tileset>
