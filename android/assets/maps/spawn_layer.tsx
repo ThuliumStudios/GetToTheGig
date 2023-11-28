b93a437ba7ec55d70de80dd6fd00eaa56684cab0
Brandin Hand <brandinfoot@gmail.com>
2023-11-27 23:42:56 -0500

    Spawn fixes
    Added properties to spawn layer objects

--- android/assets/maps/spawn_layer.tsx
+++ android/assets/maps/spawn_layer.tsx
@@ -1,10 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.6.0" name="spawn_layer" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+<tileset version="1.8" tiledversion="1.8.5" name="spawn_layer" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="spawn_layer.png" width="512" height="512"/>
  <tile id="0">
   <properties>
    <property name="height" type="float" value="0.5"/>
    <property name="spawn" value="squirrel"/>
+   <property name="type" value="enemy"/>
    <property name="width" type="float" value="0.5"/>
   </properties>
  </tile>
@@ -12,6 +13,23 @@
   <properties>
    <property name="height" type="float" value="1"/>
    <property name="spawn" value="coon"/>
+   <property name="type" value="enemy"/>
+   <property name="width" type="float" value="1"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="height" type="float" value="1"/>
+   <property name="spawn" value="guitar"/>
+   <property name="type" value="item"/>
+   <property name="width" type="float" value="1"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="height" type="float" value="1"/>
+   <property name="spawn" value="taco"/>
+   <property name="type" value="npc"/>
    <property name="width" type="float" value="1"/>
   </properties>
  </tile>
