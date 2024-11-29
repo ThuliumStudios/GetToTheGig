a8c9e00a73ce42dfa429849ea24295c28be265f3
Brandin Hand <brandinfoot@gmail.com>
2024-11-28 22:03:59 -0500

    Began the progress/preference save process

--- core/src/com/thulium/main/MainGame.java
+++ core/src/com/thulium/main/MainGame.java
@@ -1,9 +1,9 @@
 package com.thulium.main;
 
 import aurelienribon.tweenengine.Tween;
-import aurelienribon.tweenengine.TweenManager;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Preferences;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
@@ -13,27 +13,45 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.ray3k.stripe.FreeTypeSkinLoader;
 import com.thulium.screen.GameScreen;
-import com.thulium.util.Jukebox;
 import com.thulium.util.SpriteAccessor;
 
-import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainGame extends Game {
+	public enum Prefs {
+		world("world"),
+		settings("settings");
+
+		final Preferences prefs;
+		final Map<String, Object> setting = new HashMap<>();
+		Prefs(String prefs) {
+			this.prefs = Gdx.app.getPreferences(prefs);
+		}
+
+		public void saveValue(String field, Object value) {
+			setting.put(field, value);
+			prefs.put(setting);
+			prefs.flush();
+		}
+	}
 	private AssetManager assets;
 	private Skin skin;
 	private Batch batch;
 
-	private Jukebox jukebox;
-
 	@Override
 	public void create() {
-		batch = new SpriteBatch();
-		
-		skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
-		jukebox = new Jukebox();
+		// Pre-check initialization
+		Preferences worldPrefs = Gdx.app.getPreferences("world");
+		if (!worldPrefs.contains("x")) {
+			saveValue("x", 2.5f, Prefs.world);
+			saveValue("y", 2.5f, Prefs.world);
+		}
 
+		batch = new SpriteBatch();
 		Tween.registerAccessor(Sprite.class, new SpriteAccessor());
 
 		loadAssets();
@@ -64,8 +82,8 @@ public class MainGame extends Game {
 		List.of("BG_Decor", "Ground", "Middle_Decor", "snowymountains", "trees_fg", "whiteclouds")
 				.forEach(img -> assets.load("maps/" + img + ".png", Texture.class));
 
-		// Load atlas files
-		List.of("coon", "hud", "items", "particles", "player", "playerwaxe", "squirrel")
+		// Load sprite sheets (.atlas files)
+		List.of("axe", "coon", "hud", "items", "particles", "player", "playerwaxe", "scene", "squirrel")
 				.forEach(atlas -> assets.load("img/" + atlas + ".atlas", TextureAtlas.class));
 
 		// Load music
@@ -73,10 +91,20 @@ public class MainGame extends Game {
 				.forEach(m -> assets.load("raw/" + m + ".ogg", Music.class));
 
 		// Load sound effects
-		// List.of().forEach(s -> assets.load("raw/" + s + ".ogg", Sound.class));
+		List.of().forEach(s -> assets.load("raw/" + s + ".ogg", Sound.class));
+
+		// Load skin with .ttf
+		assets.setLoader(Skin.class, new FreeTypeSkinLoader(assets.getFileHandleResolver()));
+		assets.load("skin/uiskin.json", Skin.class);
+		assets.finishLoadingAsset("skin/uiskin.json");
+		skin = assets.get("skin/uiskin.json");
 
 		assets.finishLoading();
 		assets.finishLoadingAsset("img/player.atlas");
+	}
+
+	public void saveValue(String field, Object value, Prefs prefs) {
+		prefs.saveValue(field, value);
 	}
 
 	@Override
@@ -84,6 +112,6 @@ public class MainGame extends Game {
 		super.dispose();
 		assets.dispose();
 		batch.dispose();
-		skin.dispose();
+		getScreen().dispose();
 	}
 }
