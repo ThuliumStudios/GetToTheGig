934c5a7505d19b3dfef4565f295e548dbbd17690
Brandin Hand <brandinfoot@gmail.com>
2024-11-28 22:06:15 -0500

    Made Jukebox calls static

--- core/src/com/thulium/util/Jukebox.java
+++ core/src/com/thulium/util/Jukebox.java
@@ -2,22 +2,24 @@ package com.thulium.util;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 
 public class Jukebox {
-    private Music music;
+    private static Music music;
 
-    public void Jukebox() {
-
-    }
-
-    public void playMusic(float volume) {
+    public static void playMusic(float volume) {
         music = Gdx.audio.newMusic(Gdx.files.internal("raw/level_1.ogg"));
         music.setLooping(true);
         music.setVolume(volume);
         music.play();
     }
 
-    public void setVolume(float volume) {
+    public static void playSFX() {
+        Sound sfx = Gdx.audio.newSound(Gdx.files.internal("raw/level_1.ogg"));
+        sfx.play(music.getVolume());
+    }
+
+    public static void setVolume(float volume) {
         music.setVolume(volume);
     }
 }
