0e5df3c10eb8773eebceb4cfe712ce2c85458411
Brandin Hand <brandinfoot@gmail.com>
2026-03-02 18:06:10 -0500

    Jukebox updates
    Changed Jukebox to return a static instance rather than being static entirely

--- core/src/com/thulium/util/Jukebox.java
+++ core/src/com/thulium/util/Jukebox.java
@@ -5,21 +5,44 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 
 public class Jukebox {
-    private static Music music;
+    public static Jukebox instance;
+    private final Music music;
 
-    public static void playMusic(float volume) {
+    private Jukebox() {
         music = Gdx.audio.newMusic(Gdx.files.internal("raw/level_1.ogg"));
+        playMusic(1); // TODO: Get based on setting/preference
+    }
+
+    public static Jukebox getInstance() {
+        if (instance == null)
+            instance = new Jukebox();
+        return instance;
+    }
+
+    public void playMusic(float volume) {
         music.setLooping(true);
         music.setVolume(volume);
         music.play();
     }
 
-    public static void playSFX() {
+    public void playSFX() {
         Sound sfx = Gdx.audio.newSound(Gdx.files.internal("raw/level_1.ogg"));
         sfx.play(music.getVolume());
     }
 
-    public static void setVolume(float volume) {
+    public void setVolume(float volume) {
         music.setVolume(volume);
+    }
+
+    public void pause() {
+        music.pause();
+    }
+
+    public void resume() {
+        music.play();
+    }
+
+    public void stop() {
+        music.stop();
     }
 }
